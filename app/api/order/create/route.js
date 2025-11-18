18799381e66744ec8aa9a620d8c0ea3fca42823d
Huzaifa Khan <hmzkhanhmzk@gmail.com>
2025-11-18 21:59:37 +0530

    save

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,3 +1,4 @@
+import { inngest } from "@/config/inngest";
 import Product from "@/models/product";
 import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server";
@@ -24,8 +25,31 @@ export async function POST(request) {
             return acc + product.offerPrice * item.quantity;
          },0)
 
-         
+         await inngest.send({
+            name: "order/created",
+            data: {
+                userId,
+                address,
+                items,
+                amount: amount + Math.floor(amount * 0.02),
+                date: Date.now()
+            }
+         })
+
+         // clear user cart after order creation - done in inngest function
+
+         const user = await User.findById(userId)
+
+         user.cartItems = {}
+
+         await user.save()
+
+         return NextResponse.json({ success: true, message: 'Order placed successfully' }, { status: 200 })
+
     } catch (error) {
 
+        console.log(error)
+
+        return NextResponse.json({ success: false, message: error.message }, { status: 500 })
     }
 }
